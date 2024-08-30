9380c96c8118f007b72120a7cf39854afcb43f06
tmpaajsdlkjasld <167619681+tmpaajsdlkjasld@users.noreply.github.com>
2024-08-30 07:19:40 +0000

    Update main.py
    Add movement

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+from gpiozero import Servo
+#from time import sleep
 
+def left():
+	left_motor.max()
+	right_motor.max()
+
+def forwards():
+	left_motor.min()
+	right_motor.min()
+
+def right():
+	left_motor.min()
+	right_motor.min()
+
+def backwards():
+	left_motor.min()
+	right_motor.min()
+
+left_motor = Servo(25)
+right_motor = Servo(4)
+
+left_motor.value = None
+right_motor.value = None
+
+while True
+	pass
