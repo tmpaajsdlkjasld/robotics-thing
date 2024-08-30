ed7ba59341145c424e1dae8ab9f1d2ac1aa530db
tmpaajsdlkjasld <167619681+tmpaajsdlkjasld@users.noreply.github.com>
2024-08-30 07:30:53 +0000

    Update main.py
    Add camera functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gpiozero import Servo
+from picamera2 import Picamera2
 #from time import sleep
 
 def left():
@@ -23,5 +24,24 @@ right_motor = Servo(4)
 left_motor.value = None
 right_motor.value = None
 
+picam2 = Picamera2()
+picam2.start()
+
 while True
-	pass
+	array = picam2.capture_array()
+	r = 0
+	g = 0
+	b = 0
+	for x in range(480):
+		for y in range(640):
+			r += array[x][y][0]
+			g += array[x][y][1]
+			b += array[x][y][2]
+	if r > 307200*20 and g > 307200:
+		right()
+	elif r > 307200*20:
+		backwards()
+	elif g > 307200*20:
+		forwards()
+	elif b > 307200*20:
+		left()
